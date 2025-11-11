8b091ceb01774c9fefb5aa04edc6979377cb89d0
Aya <ayaadrif237@gmail.com>
2025-11-11 16:51:32 +0100

    premiere initialisation

---
+++ script.js
@@ -0,0 +1,17 @@
+function load_data() {
+  fetch("data.json") //Charge le fichier JSON
+    .then((response) => response.json()) //Convertit la réponse en format JSON
+    .then((data) => {
+      const students = data.std; //Récupère le tableau des etudiants
+      const studentList = document.getElementById("studentList"); //ul
+      studentList.innerHTML = ""; //Vide la liste avant de l'afficher
+
+      //Parcours du tableau des étudiants
+      students.forEach(student => {
+        const listItem = document.createElement("li");
+        listItem.textContent = student.name; //Affiche le nom de l'étudiant
+        studentList.appendChild(listItem); //Ajoute l'élement à la liste
+      });
+    })
+    .catch(error => console.error("Erreur :", error)); //Gestion des erreurs
+}
